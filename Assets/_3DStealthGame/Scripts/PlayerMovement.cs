20586344cf366fec61c31e7102fb7e9ef6225317
liberty1987c <liberty1987c@gmail.com>
2025-07-24 17:54:52 -0500

    Sistema animacion player

--- Assets/_3DStealthGame/Scripts/PlayerMovement.cs
+++ Assets/_3DStealthGame/Scripts/PlayerMovement.cs
@@ -5,6 +5,9 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+ Animator m_Animator;
+
     public InputAction MoveAction;
 
     public float walkSpeed = 1.0f;
@@ -14,10 +17,11 @@ public class PlayerMovement : MonoBehaviour
     Vector3 m_Movement;
     Quaternion m_Rotation = Quaternion.identity;
 
-    void Start ()
+    void Start()
     {
-        m_Rigidbody = GetComponent<Rigidbody> ();
+        m_Rigidbody = GetComponent<Rigidbody>();
         MoveAction.Enable();
+        m_Animator = GetComponent<Animator> ();
     }
 
     void FixedUpdate ()
@@ -29,6 +33,11 @@ public class PlayerMovement : MonoBehaviour
         
         m_Movement.Set(horizontal, 0f, vertical);
         m_Movement.Normalize ();
+
+        bool hasHorizontalInput = !Mathf.Approximately (horizontal, 0f);
+       bool hasVerticalInput = !Mathf.Approximately (vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+       m_Animator.SetBool ("IsWalking", isWalking);
 
         Vector3 desiredForward = Vector3.RotateTowards (transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
         m_Rotation = Quaternion.LookRotation (desiredForward);
